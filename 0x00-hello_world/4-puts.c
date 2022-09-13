1f0b4386f5025f4551071af8337b3b12c17ed9b5
ThubelihleMthwane <thubelihlemthwane@gmail.com>
2022-09-13 05:22:03 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *
- **Description: Main - Prints out a string to stdout.
- *
- **return: 0 if success
- *
- **/
+*Main - print the string in the put function
+*
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzle
+* return: 0
+*/
 
 int main(void)
 {
