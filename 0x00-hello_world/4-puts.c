0bc87613cf8104775713d1a91382b003dc4cb4a5
ThubelihleMthwane <thubelihlemthwane@gmail.com>
2022-09-13 05:06:46 -0700

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ **Description: Main - Prints out a string to stdout.
+ *
+ **return: 0 if success
+ *
+ **/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzles");
+	return (0);
+}
