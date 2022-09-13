cff4b3a38d74e5067ae13832f74c552fd10fc517
ThubelihleMthwane <thubelihlemthwane@gmail.com>
2022-09-13 05:45:00 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*Main - print the string in the put function
+*main - print the string in the put function
 *
 *Description: using the main function
 *this program prints "Programming is like building a multilingual puzzle
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzlie");
 	return (0);
 }
